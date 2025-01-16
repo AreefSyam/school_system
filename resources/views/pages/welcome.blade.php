f7267e67e99788ea1abd33cdc31a8a1dea397849
AreefSyam <aimanhisam02@gmail.com>
2025-01-17 00:52:31 +0800

    edit welcome page, buang comment

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -130,23 +130,6 @@
 
 
     <!-- Icons Grid-->
-    {{-- <section class="text-center bg-light">
-        <div
-            style="position: relative; width: 100%; height: 0; padding-top: 50%; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); overflow: hidden; border-radius: 8px; will-change: transform;">
-            <iframe loading="lazy"
-                style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
-                src="https://www.canva.com/design/DAGZwUuSFb8/G7Ser9ZkoFJNutap-8r5eg/view?embed"
-                allowfullscreen="allowfullscreen" allow="fullscreen">
-            </iframe>
-        </div>
-    </section> --}}
-
-
-
-
-
-
-    <!-- Icons Grid-->
     <section class="features-icons bg-light text-center py-5">
         <div class="container">
             <h2 class="mb-5" style="color: green; font-weight: bold; font-family: 'Poppins'"> Ikuti Perkembangan Sekolah
@@ -253,6 +236,7 @@
         </div>
     </section>
 
+    <!-- Canva Banner -->
     <section>
         <div style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
  padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); overflow: hidden;
