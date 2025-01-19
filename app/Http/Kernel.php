a28d6646c9552ef2df73fcec9f8606fcef058c87
AreefSyam <aimanhisam02@gmail.com>
2025-01-20 00:10:41 +0800

    update kernel comment

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // New Add middleware
+        // New middleware
         'remember_me_expiration' => \App\Http\Middleware\RememberMeExpiration::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'teacher' => \App\Http\Middleware\TeacherMiddleware::class,
