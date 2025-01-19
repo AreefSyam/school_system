398fd8585229437bc681a97ce1b615b7e3918274
AreefSyam <aimanhisam02@gmail.com>
2025-01-20 01:03:55 +0800

    repair syllabus model comments

--- app/Models/SyllabusModel.php
+++ app/Models/SyllabusModel.php
@@ -17,10 +17,4 @@ class SyllabusModel extends Model
         return $this->hasMany(SubjectModel::class, 'syllabus_id');
     }
 
-    // Relationship with ExamTypeModel through pivot
-    // public function examTypes()
-    // {
-    //     return $this->belongsToMany(ExamTypeModel::class, 'exam_type_syllabus_year', 'syllabus_id', 'exam_type_id')
-    //         ->withPivot('academic_year_id');
-    // }
 }
